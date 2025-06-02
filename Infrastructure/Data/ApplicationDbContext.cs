e9eb9a649c51d60f24d43140affe10452b4ea2bc
WinWrap <kima4@elev.tec.dk>
2025-06-02 22:52:13 +0200

    deletebook fluentapi

--- Infrastructure/Data/ApplicationDbContext.cs
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -1,12 +1,22 @@
 ﻿using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
 
 namespace Infrastructure.Data
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Book>()
+    .HasOne(b => b.Cover)
+    .WithOne(c => c.Book)
+    .HasForeignKey<Cover>(c => c.BookId)
+    .OnDelete(DeleteBehavior.Cascade);
         }
         public DbSet<Book> Books { get; set; }
         public DbSet<Artist> Artists { get; set; }
